cfea466e0ec6659c36415272b09eb75eb722e2ad
Anugrah James <anugrahjames2006@gmail.com>
2024-05-05 22:36:04 +0530

    Day 2 Completed

---
+++ for_sample.py
@@ -0,0 +1,9 @@
+movies= ["The Matrix.mp4", "Pirates of Silicon Valley.mp4", "The Social Network,mp4", "Mr. Robot.mp4", "Silicon Valley.mp4","Who Am I.mp4", "Snowden.mp4"]
+
+print(movies)
+
+for movie in movies:
+    print(movie[0:-4])
+
+for i in range(0,12,2):
+    print(i)
